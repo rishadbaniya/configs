9cddf18ab77da890715e90724fe0134e85ae172e
rishadbaniya <rbdroid01@gmail.com>
2023-03-07 14:58:12 +0545

    Added COC options scroll through on TAB and SHIFT+TAB and selection on ENTER

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,48 @@
 local keymap = vim.keymap;
+local fn = vim.fn
 
-keymap.set("n", "<M-1>", ":NERDTreeToggle<CR>",{noremap = true})
+function check_b_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+keymap.set("n", "<M-1>", ":NERDTreeToggle<CR>",{ noremap = true })
+
+-- Use of <CR> to trigger completion, when COC Options are visible
+keymap.set('i', '<CR>', function()
+    if fn["coc#pum#visible"]() == 1 then
+        return fn["coc#pum#confirm"]()
+    else
+        return "<CR>"
+    end
+end, { noremap = true, expr = true})
+
+-- Use of <TAB> to scroll through the provided COC Options
+keymap.set("i", "<TAB>", function()
+    if fn["coc#pum#visible"]() == 1 then
+        return fn["coc#pum#next"](1)
+    else
+        if check_b_space() == 1 then
+            return "<TAB>"
+        else
+            return fn["coc#refresh"]()
+        end
+    end
+end, {silent = true, noremap = true, expr = true})
+
+-- Use of <SHIFT-TAB> to scroll upward through the provided COC Options
+keymap.set("i", "<S-TAB>", function()
+    if fn["coc#pum#visible"]() == 1 then
+        return fn["coc#pum#prev"](1)
+    else
+        return "<C-h>"
+    end
+end, {silent = true, noremap = true, expr = true})
+
+--keymap.set("n", "gn" ,"<Plug>coc-diagnostics-next")
+--keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
+---- Toggle CocDiagnostics i.e list all errors/warnings thrown by LSP
+--local toggle_coc_diagnostics = function()
+   --local is_enabled = fn["Coca"]
+--end
+
--- nvim/lua/packer_config.lua
+++ nvim/lua/packer_config.lua
@@ -2,19 +2,27 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use { "wbthomason/packer.nvim" }
-    -- LSp
-	use {"neoclide/coc.nvim", branch = "release" }
+	use { "wbthomason/packer.nvim" } 
+    --LSP 
+    use {"neoclide/coc.nvim", branch = "release" }
 
     -- Enhancements
     use {"preservim/nerdcommenter"}
     use {"itchyny/lightline.vim"}
-    use {"jiangmiao/auto-pairs"}
     use {"dstein64/nvim-scrollview", branch = 'main' }
     use {'Yggdroot/indentLine'}
     use {"ryanoasis/vim-devicons"}
     use {"preservim/nerdtree"}
-
+    use {
+	   "windwp/nvim-autopairs",
+       config = function() require("nvim-autopairs").setup {
+           map_cr = false
+       } end
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 	-- Theme
 	use { "ellisonleao/gruvbox.nvim" }
 end)
