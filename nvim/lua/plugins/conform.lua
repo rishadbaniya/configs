ceb331a642331293dd3664922d121f0a1b9e2423
rishadbaniya <rbdroid01@gmail.com>
2024-06-01 18:15:12 +0545

    [conform] fix: add eslint_d for formatting i.e to use the formatting rules from the eslint configuration file

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,30 +1,33 @@
--- One would have to install them by opening :Mason
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	opts = {
 		formatters_by_ft = {
-			javascript = { { "eslint", "prettier" } },
-			typescript = { { "eslint" } },
+			javascript = { { "eslint_d" } },
+			typescript = { { "eslint_d" } },
+			typescriptreact = { { "eslint_d" } }, -- https://github.com/stevearc/conform.nvim/issues/218#issuecomment-1826438350
 			ocaml = { "ocamlformat" },
+			c = { { "clang-format" } },
+			xml = { { "xmlformat" } },
 			json = { "jq" },
 			lua = { "stylua" },
-			rust = { "rustfmt" },
-			typescriptreact = { { "eslint_d", "eslint" } },
+			html = { "htmlbeautifier" },
 			["*"] = { "trim_whitespace", "squeeze_blanks", "trim_newlines" },
 		},
 		format_on_save = function(bufnr)
-			-- Disable autoformat for files in a certain path
 			local bufname = vim.api.nvim_buf_get_name(bufnr)
 			if bufname:match("/node_modules/") or bufname:match("/vendor/") then
 				return
 			end
 			return { async = true, timeout_ms = 500, lsp_fallback = true }
 		end,
+		log_level = vim.log.levels.ERROR,
+		notify_on_error = true,
 	},
 	config = function(_, opts)
 		local cs_fixer = require("conform.formatters.php_cs_fixer")
+
 		cs_fixer.args = function(self, ctx)
 			local args = { "fix", "$FILENAME", "--quiet", "--no-interaction", "--using-cache=no" }
 			local found = vim.fs.find(".php-cs-fixer.php", { upward = true, path = ctx.dirname })[1]
