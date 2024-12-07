daad9fbbfece9d677e195f45e30230e1d8c5d503
rishadbaniya <rbdroid01@gmail.com>
2024-12-07 23:19:56 +0545

    feat(nvim): add barbar tabline utility for tab management

---
+++ nvim/lua/plugins/barbar.lua
@@ -0,0 +1,19 @@
+local opts = { noremap = true, silent = true }
+return {
+	"romgrk/barbar.nvim",
+	dependencies = {
+		"lewis6991/gitsigns.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	version = "^1.0.0",
+	init = function()
+		vim.g.barbar_auto_setup = false
+	end,
+	opts = {},
+	config = function()
+		require("barbar").setup()
+		-- Move to previous/next
+		vim.api.nvim_set_keymap("n", "<C-h>", "<Cmd>BufferPrevious<CR>", opts)
+		vim.api.nvim_set_keymap("n", "<C-l>", "<Cmd>BufferNext<CR>", opts)
+	end,
+}
