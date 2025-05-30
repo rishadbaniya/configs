f8d68a2920e8a114864bce004d4cc35735915f0a
rishadbaniya <rbdroid01@gmail.com>
2025-05-30 22:07:26 +0545

    fix(nvim/trouble): add correct command for trouble

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -3,9 +3,7 @@ return {
 	cmd = { "TroubleToggle", "Trouble" },
 	opts = { use_diagnostic_signs = true },
 	keys = {
-		{ "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-		{ "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-		{ "<leader>tl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-		{ "<leader>tf", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+		{ "<leader>tw", "<cmd>Trouble diagnostics toggle<cr>", desc = "Workspace Diagnostics (Trouble)" },
+		{ "<leader>tl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
 	},
 }
